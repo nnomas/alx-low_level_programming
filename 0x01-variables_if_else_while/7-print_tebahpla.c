72667917d2a98f02a3eb28e56f00d4501ab6b715
nnomas <samsamas754@gmail.com>
2023-03-17 16:11:29 -0700

    Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Description: using ASCII to print lowercase letters in revers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
