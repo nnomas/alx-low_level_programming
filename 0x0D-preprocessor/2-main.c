9e9cca4672f49a4914c0426cb4c0e6d7109ed78a
nnomas <samsamas754@gmail.com>
2023-04-17 13:13:08 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+/* This is the main function of my program. It takes no arguments and
+ * returns an integer value indicating the program's exit status.
+*/
+
+printf("This program was compiled from the file: %s\n", __FILE__);
+return (0);
+}
+
