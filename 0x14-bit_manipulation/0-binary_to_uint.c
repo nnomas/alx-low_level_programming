7db3064d26699b824c90bc71be13b0487153c770
Sam Samas <samsamas754@gmail.com>
2023-07-07 13:14:14 -0700

    Read me

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#inlcude "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * *@b: binary
+ *
+ * Return: unsigned int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i;
+	int len, b2;
+
+	if (!b)
+		return (0);
+
+	i = 0;
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, b2 = 1; len >= 0; len--, b2 *= 2)
+	{
+		if (b[len] != '\0' && b[len] != '1')
+			return (0);
+
+		if (b[len] & 1)
+			i += b2;
+	}
+	return (i);
+}
